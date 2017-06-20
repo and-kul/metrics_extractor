acc4f43616d1c27c8d3452d33daeadfb3c983e31
Andrey Kulagin <mr.and.kul@gmail.com>
2017-06-20 14:26:36 +0300

    Update creating tables

--- creating_tables.sql
+++ creating_tables.sql
@@ -1,5 +1,6 @@
+DROP TABLE IF EXISTS functions;
+DROP TABLE IF EXISTS regions;
 DROP TABLE IF EXISTS files;
-DROP TABLE IF EXISTS statistics;
 DROP TABLE IF EXISTS languages;
 DROP TABLE IF EXISTS projects;
 
@@ -17,31 +18,48 @@ CREATE TABLE languages (
 
 
 CREATE TABLE files (
-  id SERIAL PRIMARY KEY,
-  project_id INTEGER NOT NULL REFERENCES projects (id),
-  path TEXT NOT NULL,
-  language_name TEXT NOT NULL REFERENCES languages (name),
-  cloc_blank_lines INTEGER,
+  id                 SERIAL PRIMARY KEY,
+  project_id         INTEGER NOT NULL REFERENCES projects (id),
+  path               TEXT    NOT NULL,
+  language_name      TEXT    NOT NULL REFERENCES languages (name),
+  cloc_blank_lines   INTEGER,
   cloc_comment_lines INTEGER,
-  cloc_code_lines INTEGER
+  cloc_code_lines    INTEGER
 );
 
 
-CREATE TABLE statistics (
-  project_id INTEGER NOT NULL REFERENCES projects (id) ON DELETE CASCADE,
-  language_name TEXT NOT NULL REFERENCES languages (name) ON UPDATE CASCADE ON DELETE CASCADE,
+CREATE TABLE regions (
+  file_id INTEGER NOT NULL REFERENCES files (id),
+  region_id      INTEGER NOT NULL,
+  PRIMARY KEY (file_id, region_id),
+  type TEXT NOT NULL,
+  outer_region_id INTEGER,
+  FOREIGN KEY (file_id, outer_region_id) REFERENCES regions (file_id, region_id),
+  total_lines INTEGER,
   code_lines INTEGER,
-  cloc_code_lines INTEGER,
-  comment_lines INTEGER,
   blank_lines INTEGER,
-  functions INTEGER,
-  avg_ccn_per_function DOUBLE PRECISION,
-  avg_code_lines_per_function DOUBLE PRECISION,
-  files INTEGER,
-  cloc_files INTEGER,
-
-  PRIMARY KEY (project_id, language_name)
+  comment_lines INTEGER,
+  average_cyclomatic_complexity DOUBLE PRECISION,
+  n_functions INTEGER
 );
+
+CREATE TABLE functions (
+  file_id INTEGER NOT NULL REFERENCES files (id),
+  region_id INTEGER NOT NULL,
+  function_id INTEGER NOT NULL,
+  PRIMARY KEY (file_id, function_id),
+  FOREIGN KEY (file_id, region_id) REFERENCES regions (file_id, region_id),
+  total_lines INTEGER,
+  code_lines INTEGER,
+  blank_lines INTEGER,
+  comment_lines INTEGER,
+  cycomatic_complexity INTEGER
+);
+
+
+
+
+
 
 INSERT INTO languages (name) VALUES
   ('Java'),
